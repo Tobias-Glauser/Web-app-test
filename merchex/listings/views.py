a5a6ccda839d616899706262b3e73bf0a9b4c3e5
Tobias-Glauser <tobiasglauser2006@gmail.com>
2022-04-08 22:03:05 +0200

    fisrt commit

---
+++ merchex/listings/views.py
@@ -0,0 +1,16 @@
+
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def hello(request):
+    return HttpResponse('<h1>Hello Django !</h1>')
+
+def about(request):
+    return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
+
+def listings(request):
+    return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
+
+def contact(request):
+    return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
